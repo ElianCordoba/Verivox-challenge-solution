f13d0a6e55a47fc93f9a7f7eb6935a46ae1e51a0
Elian Cordoba <eliancordoba98@gmail.com>
2019-02-03 12:52:23 +0000

    Fixed icon and error handling issues

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
+import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
+import favicon from 'serve-favicon';
+import path from 'path';
 
 import consumptionRouter from './routes/consumption';
 
@@ -8,9 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, '../', 'public', 'favicon.ico')));
 
+app.use('/truth', (req: Request, res: Response) => res.status(418).send('Elian === Cool dev'));
 app.use('/consumption', consumptionRouter);
 
+app.use(errors());
 app.use((err: any, req: Request, res: Response) => {
   res.locals.message = err.message;
   return res.status(err.status || 500).send(err.details);
